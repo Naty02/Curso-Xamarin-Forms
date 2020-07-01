599f749200fd561ad8f284c7305456775d10b7fb
Naty02 <fnaty629@gmail.com>
2020-07-01 01:36:34 -0400

    ApiHamb
    WebApi

---
+++ Services/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnicornDemoApi.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private UnicornDemoContext NetCoreX;
+         private BaseRepository<Usuario> _user;
+        private BaseRepository<Contacto> _contact;
+
+        public UnitOfWork(UnicornDemoContext dbContext)
+        {
+            dbContext = DbContext;
+        }
+
+        public IRepository<Usuario> Usuarios
+             {
+            get
+            { 
+                return _usuario ?? (_usuario = new BaseRepository<User>(dbContext));
+
+            }
+        }
+
+        public IRpository<Contacto> Contactos
+         {
+            get
+             {
+                return _contact ??
+                                    (_contacto = new BaseRepository<Contact>(dbContext));
+
+            }
+        }
+
+         public void Save()
+        {
+            dbContext.SaveChanges();
+
+        }
+
+}
+}
